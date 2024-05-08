d8c6aac4772e4f2264e3733df3f0b7e6d8074e6f
Nahom kiflu <kiflunahom1994@gmail.com>
2024-05-08 23:56:19 +0300

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creating tabel user
+-- unique id 
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+)
