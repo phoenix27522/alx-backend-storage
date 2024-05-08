d9d2532befc76674c5d61779cdb31f21d174cad3
Nahom kiflu <kiflunahom1994@gmail.com>
2024-05-08 23:51:52 +0300

    task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- creating tabel user
+-- unique id 
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+)
